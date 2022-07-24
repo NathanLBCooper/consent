79b62697f18d2eb58e31bb08ad5d119badb79df3
nathanlbcooper <nathan.lb.cooper@outlook.com>
2022-07-24 19:39:30 +0200

    Start to implement accountController

--- api/internal/server/accountController.go
+++ api/internal/server/accountController.go
@@ -6,6 +6,10 @@ import (
 	"nathan.dev/consent/internal/domain"
 )
 
+type organizationCreateRequest struct {
+	Name string
+}
+
 type accountController struct {
 	endpoint *domain.AccountEndpoint
 	session  sessionStorage
@@ -23,13 +27,13 @@ func newAccountController(endpoint *domain.AccountEndpoint, session sessionStora
 func (c *accountController) userGet(ctx *gin.Context) {
 	id, err := uuid.Parse(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(400, gin.H{"status": "todo bad id"})
+		ctx.JSON(400, gin.H{"status": "id parameter is not valid"})
 		return
 	}
 
 	model, err := c.endpoint.UserGet(newServerContext(ctx), domain.UserId(id))
 	if err != nil {
-		ctx.JSON(404, gin.H{"status": "not found"})
+		ctx.JSON(404, gin.H{"status": "user not found"})
 		return
 	}
 
@@ -43,13 +47,13 @@ func (c *accountController) userGet(ctx *gin.Context) {
 func (c *accountController) userCreate(ctx *gin.Context) {
 	var request domain.User
 	if err := ctx.BindJSON(&request); err != nil {
-		ctx.JSON(500, gin.H{"status": "todo 1"})
+		ctx.JSON(400, gin.H{"status": "request body is not valid"})
 		return
 	}
 
 	model, err := c.endpoint.UserCreate(newServerContext(ctx), request)
 	if err != nil {
-		ctx.JSON(500, gin.H{"status": "todo 2"})
+		ctx.JSON(500, gin.H{"status": "error creating user"})
 		return
 	}
 
@@ -61,11 +65,19 @@ func (c *accountController) userCreate(ctx *gin.Context) {
 // @Success      200  {object}  domain.OrganizationModel
 // @Router       /v1/account/organization/{id} [get]
 func (c *accountController) organizationGet(ctx *gin.Context) {
-	ctx.JSON(500, gin.H{"status": "not implemented"})
-}
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(400, gin.H{"status": "id parameter is not valid"})
+		return
+	}
 
-type organizationCreateRequest struct {
-	Name string
+	model, err := c.endpoint.OrganizationGet(newServerContext(ctx), domain.OrganizationId(id))
+	if err != nil {
+		ctx.JSON(404, gin.H{"status": "organization not found"})
+		return
+	}
+
+	ctx.JSON(200, gin.H{"status": model})
 }
 
 // @Summary      Create an Organization
@@ -73,18 +85,29 @@ type organizationCreateRequest struct {
 // @Success      200  {object}  domain.OrganizationModel
 // @Router       /v1/account/organization [post]
 func (c *accountController) organizationCreate(ctx *gin.Context) {
-	context := newServerContext(ctx)
-	id := c.session.Get(context.CorrelationId(), "user")
-	if id == "" {
-		ctx.JSON(401, gin.H{"status": "not logged in"})
-		return
-	}
+	//context := newServerContext(ctx)
+	// userId := c.session.Get(context.CorrelationId(), "user")
+	// if userId == "" {
+	// 	ctx.JSON(401, gin.H{"status": "requesting user is not logged in"})
+	// 	return
+	// }
+
+	// todo get userid
+	userId := uuid.New()
 
 	var request organizationCreateRequest
 	if err := ctx.BindJSON(&request); err != nil {
-		ctx.JSON(500, gin.H{"status": "todo 1"})
+		ctx.JSON(500, gin.H{"status": "request body is not valid"})
 		return
 	}
 
-	ctx.JSON(500, gin.H{"status": "not implemented"})
+	createRequest := domain.OrganizationCreateRequest{Name: request.Name, OwnerUserId: domain.UserId(userId)}
+
+	model, err := c.endpoint.OrganizationCreate(newServerContext(ctx), createRequest)
+	if err != nil {
+		ctx.JSON(500, gin.H{"status": "error creating organization"})
+		return
+	}
+
+	ctx.JSON(200, gin.H{"status": model})
 }
