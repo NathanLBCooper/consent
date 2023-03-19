05dd4ca5ec0c2041d3992ba0dd308c255ff5e167
nathanlbcooper <nathan.lb.cooper@outlook.com>
2023-03-19 13:35:53 +0100

    Add migrations to Migrator

--- src/Consent.Storage.Migrator/Migrations/Migration001_Initial.cs
+++ src/Consent.Storage.Migrator/Migrations/Migration001_Initial.cs
@@ -8,31 +8,88 @@ internal class Migration001_Initial : Migration
     protected override void Up()
     {
         Execute(@"
-create table [dbo].[User](
-    [Id] int identity primary key,
-    [Name] nvarchar(max) not null,
-    );
+IF SCHEMA_ID(N'users') IS NULL EXEC(N'CREATE SCHEMA [users];');
 
-create table [dbo].[Workspace](
-    [Id] int identity primary key,
-    [Name] nvarchar(max) not null
-    );
+CREATE TABLE [users].[Users] (
+    [Id] int NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
 
-create table [dbo].[UserWorkspaceMembership](
-    [UserId] int foreign key references [User]([Id]),
-    [WorkspaceId] int foreign key references [Workspace]([Id]),
-    [Permission] int not null,
-    unique nonclustered ([UserId],[WorkspaceId],[Permission]) 
-    );
+
+
+
+IF SCHEMA_ID(N'workspaces') IS NULL EXEC(N'CREATE SCHEMA [workspaces];');
+
+CREATE TABLE [workspaces].[Workspaces] (
+    [Id] int NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Workspaces] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [workspaces].[Membership] (
+    [Id] int NOT NULL,
+    [User] int NOT NULL,
+    [WorkspaceId] int NULL,
+    CONSTRAINT [PK_Membership] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Membership_Workspaces_WorkspaceId] FOREIGN KEY ([WorkspaceId]) REFERENCES [workspaces].[Workspaces] ([Id])
+);
+
+CREATE INDEX [IX_Membership_WorkspaceId] ON [workspaces].[Membership] ([WorkspaceId]);
+
+
+
+
+IF SCHEMA_ID(N'contracts') IS NULL EXEC(N'CREATE SCHEMA [contracts];');
+
+CREATE TABLE [contracts].[Contracts] (
+    [Id] int NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Contracts] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [contracts].[ContractVersion] (
+    [Id] int NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    [Text] nvarchar(max) NOT NULL,
+    [Status] int NOT NULL,
+    [ContractId] int NULL,
+    CONSTRAINT [PK_ContractVersion] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ContractVersion_Contracts_ContractId] FOREIGN KEY ([ContractId]) REFERENCES [contracts].[Contracts] ([Id])
+);
+
+CREATE TABLE [contracts].[Provision] (
+    [Id] int NOT NULL,
+    [Text] nvarchar(max) NOT NULL,
+    [ContractVersionId] int NULL,
+    CONSTRAINT [PK_Provision] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Provision_ContractVersion_ContractVersionId] FOREIGN KEY ([ContractVersionId]) REFERENCES [contracts].[ContractVersion] ([Id])
+);
+
+CREATE INDEX [IX_ContractVersion_ContractId] ON [contracts].[ContractVersion] ([ContractId]);
+
+CREATE INDEX [IX_Provision_ContractVersionId] ON [contracts].[Provision] ([ContractVersionId]);
 ");
     }
 
     protected override void Down()
     {
         Execute(@"
-drop table [dbo].[User];
-drop table [dbo].[Workspace];
-drop table [dbo].[UserWorkspaceMembership];
+DROP TABLE [users].[Users];
+
+
+
+DROP TABLE [workspaces].[Membership];
+
+DROP TABLE [workspaces].[Workspaces];
+
+
+
+DROP TABLE [contracts].[Provision];
+
+DROP TABLE [contracts].[ContractVersion];
+
+DROP TABLE [contracts].[Contracts];
 ");
     }
 }
---
+++ src/Consent.Storage.Migrator/Migrations/migrations.md
@@ -0,0 +1,21 @@
+﻿How to create these from EF Core Migrations:
+
+1)  In *Consent.Storage*
+
+        \src\Consent.Storage\
+
+    Call `dotnet ef` to generate SQL from the EF Migrations.
+
+        # Generate Up
+        dotnet ef migrations script PreviousMigration NewMigration --context UserDbContext
+        dotnet ef migrations script PreviousMigration NewMigration --context WorkspaceDbContext
+        dotnet ef migrations script PreviousMigration NewMigration --context ContractDbContext
+
+        # Generate Down
+        dotnet ef migrations script NewMigration PreviousMigration --context UserDbContext
+        dotnet ef migrations script NewMigration PreviousMigration --context WorkspaceDbContext
+        dotnet ef migrations script NewMigration PreviousMigration --context ContractDbContext
+
+2) Create a new migration in this project.
+    Copy in the SQL from the previous step.
+    But discard any changes to `[__EFMigrationsHistory]`. This project doesn't use EF Migrations in the database, only to generate SQL for *Simple.Migrations*.
